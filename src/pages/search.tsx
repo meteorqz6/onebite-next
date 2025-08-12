f418a0d79a67cc3bb435286fed359d56e39f1ee9
Nam Yooseong <nys8765@naver.com>
2025-08-12 15:52:15 +0900

    2.4 프리페칭

--- src/pages/search.tsx
+++
@@ -1,9 +0,0 @@
-import { useRouter } from "next/router";
-
-export default function Page() {
-  const router = useRouter();
-  // console.log(router);
-  const { q } = router.query;
-
-  return <h1>Search {q}</h1>;
-}
