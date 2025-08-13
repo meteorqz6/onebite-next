c7523f586fa5b9b748cb8f5a5957c961222fc625
Nam Yooseong <nys8765@naver.com>
2025-08-13 20:43:59 +0900

    2.9 한입북스 UI 구현

--- src/pages/search/index.tsx
+++ src/pages/search/index.tsx
@@ -1,15 +1,18 @@
+import BookItem from "@/components/book-item";
 import SearchableLayout from "@/components/searchable-layout";
-import { useRouter } from "next/router";
 import { ReactNode } from "react";
+import books from "@/mock/books.json";
 
 export default function Page() {
-  const router = useRouter();
-  // console.log(router);
-  const { q } = router.query;
-
-  return <h1>Search {q}</h1>;
+  return (
+    <div>
+      {books.map((book) => (
+        <BookItem key={book.id} {...book} />
+      ))}
+    </div>
+  );
 }
 
 Page.getLayout = (page: ReactNode) => {
-  return <SearchableLayout>{page}</SearchableLayout>
+  return <SearchableLayout>{page}</SearchableLayout>;
 };
