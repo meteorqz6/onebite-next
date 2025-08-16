986addeb8e95bb18d54368cf325c3117bb343e3b
Nam Yooseong <nys8765@naver.com>
2025-08-16 18:39:40 +0900

    2.11 ssr 1.소개 및 실습

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,34 @@
 // CSS Module
-import { ReactNode } from "react";
+import { ReactNode, useEffect } from "react";
 import style from "./index.module.css";
 import SearchableLayout from "@/components/searchable-layout";
 import books from "@/mock/books.json";
 import BookItem from "@/components/book-item";
+import { InferGetServerSidePropsType } from "next";
 
-export default function Home() {
+export const getServerSideProps = () => {
+  // 컴포넌트보다 먼저 실행돼서 컴포넌트에 필요한 데이터 불러오는 함수
+  // 서버 측에서만 실행된다 (브라우저 X)
+  console.log("server side props");
+  const data = "hello";
+
+  return {
+    props: {
+      data,
+    },
+  };
+};
+
+export default function Home({
+  data,
+}: InferGetServerSidePropsType<typeof getServerSideProps>) {
+  // 서버에서 1번, 브라우저에서 1번 출력
+  console.log(data);
+
+  useEffect(() => {
+    console.log(window);
+  }, []);
+
   return (
     <div className={style.container}>
       <section>
